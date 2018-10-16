24d144b4bb25a16dab4ef863b40b71eec40bc7fc
Zachary Bell <zacharybell@live.com>
2018-10-15 22:16:03 -0600

    Add --append as a command line option
    This flag defaults determines if existing file will be
    overwritten or appended to on the server.

--- file-transfer-threaded/file_client.py
+++ file-transfer-threaded/file_client.py
@@ -6,9 +6,11 @@ import argparse, socket
 
 parser = argparse.ArgumentParser('Run a file transfer client.')
 
-parser.add_argument('--host', help='default 127.0.0.1', metavar='host', default='127.0.0.1', type=str)
-parser.add_argument('--port', help='default 12345', metavar='port', default=12345, type=int)
+parser.add_argument('--host', help='default 127.0.0.1', default='127.0.0.1', type=str)
+parser.add_argument('--port', help='default 12345', default=12345, type=int)
 parser.add_argument('--file', help='the path to the file', metavar='file_path', type=str)
 parser.add_argument('--name', help='the requested name for the file to be saved as', metavar='file_name', type=str)
+parser.add_argument('--append', help='appends to the end of the file if file exists', default=False, action='store_true')
 
 args = parser.parse_args()
+
